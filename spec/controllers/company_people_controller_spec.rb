f208059028147290c3e8dd3663822e941809b5e9
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-16 03:31:28 +0530

    Add rspec tests for company_people_controller

--- spec/controllers/company_people_controller_spec.rb
+++ spec/controllers/company_people_controller_spec.rb
@@ -1,19 +1,149 @@
 require 'rails_helper'
 include CompanyPeopleViewer
 
+RSpec.shared_examples "unauthorized" do
+  before :each do
+    warden.set_user user
+    request
+  end
+
+  it 'redirects to the home page' do
+    expect(response).to redirect_to(root_path)
+  end
+
+  it 'sets the flash' do
+    expect(flash[:alert]).to match(/^You are not authorized to/)
+  end
+end
+
+RSpec.shared_examples "unauthenticated" do
+  before do
+    request
+  end
+
+  it 'redirects to the home page' do
+    expect(response).to redirect_to(root_path)
+  end
+
+  it 'sets the flash' do
+    expect(flash[:alert]).to match(/You need to login to/)
+  end
+end
+
 RSpec.describe CompanyPeopleController, type: :controller do
   describe "GET #edit_profile" do
-    before(:each) do
-      @company_person = FactoryGirl.create(:company_person)
-      sign_in @company_person
-      get :edit_profile, id: @company_person
+    describe 'authorized access' do
+      context 'company admin' do
+        before(:each) do
+          @company_admin = FactoryGirl.create(:company_admin)
+          sign_in @company_admin
+          get :edit_profile, id: @company_admin
+        end
+
+        it "renders edit_profile template" do
+          expect(response).to render_template 'edit_profile'
+        end
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'company contact' do
+        before(:each) do
+          @company_contact = FactoryGirl.create(:company_contact)
+          sign_in @company_contact
+          get :edit_profile, id: @company_contact
+        end
+
+        it "renders edit_profile template" do
+          expect(response).to render_template 'edit_profile'
+        end
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+      end
     end
 
-    it "renders edit_profile template" do
-      expect(response).to render_template 'edit_profile'
-    end
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    describe 'unauthorized access' do
+      let(:company)        { FactoryGirl.create(:company) }
+      let(:company_admin) { FactoryGirl.create(:company_admin, company: company) }
+      let(:company_contact) { FactoryGirl.create(:company_contact, company: company) }
+      let(:agency)         { FactoryGirl.create(:agency) }
+      let(:agency_admin)   { FactoryGirl.create(:agency_admin, agency: agency) }
+      let(:job_developer)  { FactoryGirl.create(:job_developer, agency: agency) }
+      let(:case_manager)   { FactoryGirl.create(:case_manager, agency: agency) }
+      let(:job_seeker)     { FactoryGirl.create(:job_seeker) }
+
+      context 'not logged in' do
+        context 'company_admin' do
+          let(:request) {  get :edit_profile, id: company_admin }
+          it_behaves_like 'unauthenticated'
+        end
+        context 'company_contact' do
+          let(:request) {  get :edit_profile, id: company_contact }
+          it_behaves_like 'unauthenticated'
+        end
+      end
+
+      context 'Agency people' do
+        context 'company_admin' do
+          let(:request) {  get :edit_profile, id: company_admin }
+          it_behaves_like "unauthorized" do
+            let(:user) { agency_admin }
+          end
+          it_behaves_like "unauthorized" do
+            let(:user) { job_developer }
+          end
+          it_behaves_like "unauthorized" do
+            let(:user) { case_manager }
+          end
+        end
+
+        context 'company_contact' do
+          let(:request) {  get :edit_profile, id: company_contact }
+          it_behaves_like "unauthorized" do
+            let(:user) { agency_admin }
+          end
+          it_behaves_like "unauthorized" do
+            let(:user) { job_developer }
+          end
+          it_behaves_like "unauthorized" do
+            let(:user) { case_manager }
+          end
+        end
+      end
+
+      context 'Job Seeker' do
+        context 'Company admin' do
+          let(:request) {  get :edit_profile, id: company_admin }
+
+          it_behaves_like "unauthorized" do
+            let(:user) { job_seeker }
+          end
+        end
+
+        context 'Company contact' do
+          let(:request) {  get :edit_profile, id: company_contact }
+
+          it_behaves_like "unauthorized" do
+            let(:user) { job_seeker }
+          end
+        end
+      end
+
+      context 'Company admin' do
+        let(:request) { get :edit_profile, id: company_contact }
+        it_behaves_like "unauthorized" do
+          let(:user) { company_admin }
+        end
+      end
+
+      context 'Company contact' do
+        let(:request) { get :edit_profile, id: company_admin }
+        it_behaves_like "unauthorized" do
+          let(:user) { company_contact }
+        end
+      end
     end
   end
 
